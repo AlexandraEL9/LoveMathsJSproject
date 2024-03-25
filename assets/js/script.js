1a075c1201be523125e935414710a6821d64cc2b
AlexandraEL9 Lee <alexandraelee9@gmail.com>
2024-03-25 13:46:26 +0000

    feat:set up structure of game functions in KS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,32 @@
-console.log("COnnected!");
+function runGame(){
+
+}
+
+function checkAnswer(){
+
+}
+
+function calculateCorrectAnswer(){
+
+}
+
+function incermentScore(){
+
+}
+
+function incrementWrongAnswer(){
+
+}
+
+function displayAdditionQuestion(){
+
+}
+function displaySubtractQuestion(){
+    
+}
+function displayMultiplyQuestion(){
+    
+}
+function displayDivideQuestion(){
+    
+}
